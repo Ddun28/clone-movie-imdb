2d71ecbf07ae26b6a33edc601f8d708f49a42d2a
Daniel Dun <danieldum28@gmail.com>
2024-03-26 18:44:58 -0400

    add card the movie

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,13 +1,11 @@
-export default function Results({ results}) {
+import Card from './Card';
+
+export default function Results({ results }) {
   return (
-    <div>
-        {
-            results.map((result) => (
-                <div hey={result.id}>
-                    <h2>{result.original_title}</h2>
-                </div>
-            ))
-        }
+    <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4'>
+      {results.map((result) => (
+        <Card key={result.id} result={result} />
+      ))}
     </div>
-  )
+  );
 }
