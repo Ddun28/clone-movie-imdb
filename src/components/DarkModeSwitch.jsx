7245e39e5212f4262ccf56074b270e06f73581ed
Daniel Dun <danieldum28@gmail.com>
2024-03-31 14:32:48 -0400

    add

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -1,29 +1,29 @@
-'use client'
+'use client';
 
-import { MdLightMode, MdDarkMode } from "react-icons/md";
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
+import { MdLightMode, MdDarkMode } from 'react-icons/md';
+
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 export default function DarkModeSwitch() {
- 
-  const{theme, setTheme, systemTheme} = useTheme();
-  const currentTheme = theme === 'system' ? systemTheme : theme;
+  const { theme, setTheme, systemTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+  const currentTheme = theme === 'system' ? systemTheme : theme;
   useEffect(() => setMounted(true), []);
-
   return (
     <div>
-      {mounted && 
+      {mounted &&
         (currentTheme === 'dark' ? (
-    <MdLightMode 
-    onClick={()=>setTheme('light')} 
-    className="text-xl cursor-pointer hover:text-amber-500" 
-    /> ) : ( 
-    <MdDarkMode 
-    onClick={()=>setTheme('dark')} 
-    className="text-xl cursor-pointer hover:text-amber-500"
-      /> 
-      ))} 
-      </div>
+          <MdLightMode
+            onClick={() => setTheme('light')}
+            className='text-xl cursor-pointer hover:text-amber-500'
+          />
+        ) : (
+          <MdDarkMode
+            onClick={() => setTheme('dark')}
+            className='text-xl cursor-pointer hover:text-amber-500'
+          />
+        ))}
+    </div>
   );
 }
