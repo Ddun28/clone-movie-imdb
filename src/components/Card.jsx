460e2f973fdbe55e0294f55ba8302200f6d076d0
Daniel Dun <danieldum28@gmail.com>
2024-03-31 18:13:32 -0400

    agg changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,6 +12,7 @@ export default function Card({ result }) {
           }`}
           width={500}
           height={300}
+          alt='card-img'
           className='sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300'
         ></Image>
         <div className='p-2'>
