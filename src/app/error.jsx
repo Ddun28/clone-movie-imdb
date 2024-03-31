9232e44984a86107490ce2303352eb52f2139b4e
Daniel Dun <danieldum28@gmail.com>
2024-03-31 14:00:28 -0400

    corrections

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,14 +1,16 @@
-'use client'
+'use client';
 
-import { useEffect } from "react"
-export default function error({error, reset}) {
-    useEffect(()=> {
-        console.log(error);
-    }, [error])
+import { useEffect } from 'react';
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
   return (
-    <div className="text-center mt-10">
-    <h1>Lo siento ha ocurrido un error, intenta mas tarde</h1>
-        <button className="hover:text-amber-600" onClick={()=>reset()}>Reintentar</button>
-        </div>
-  )
+    <div className='text-center mt-10'>
+      <h1>Something went wrong. Please try again later.</h1>
+      <button className='hover:text-amber-600' onClick={() => reset()}>
+        Try Again
+      </button>
+    </div>
+  );
 }
